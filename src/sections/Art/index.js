83c0f99d8b266ec1de92851f91b0ee6dd7af2528
Manoj kumar <manoj.bhoosani@gmail.com>
2023-01-25 21:19:28 +0100

    Art and user part completed

--- src/sections/Art/index.js
+++ src/sections/Art/index.js
@@ -1,10 +1,42 @@
 import { useEffect, useState } from "react"
 
 function ArtsSection() {
+const [artworks, setArtWorks] = useState([])
+
+
+useEffect(() => {
+  fetch(`https://api.artic.edu/api/v1/artworks?limit =10`)
+  .then(response => response.json())
+  .then((data) => {
+    console.log("artwork data:", data)
+    setArtWorks(data.data)
+  })
+},[])
   return (
     <section>
       <h2>Arts Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+      <ul className="art-list">
+        {artworks.map((artwork,index)  => (
+      <li key ={index}>
+        <div className="frame">
+          <img
+            src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg` } alt = {artwork.title}
+          />
+      </div>
+      <h3>{artwork.title}</h3>
+      <p>Artist:{artwork.Artist_title}</p>
+      <h4>Artistic Subjects:</h4>
+      <ul>
+      {artwork.subject_titles.map((subject, index) => (
+        <li key= {index}>{subject}</li>
+      ))}
+           
+      </ul>
+      </li>
+     ))}
+      </ul>
+      </div>
     </section>
   )
 }
--- src/sections/Users/index.js
+++ src/sections/Users/index.js
@@ -1,10 +1,32 @@
 import { useEffect, useState } from "react"
 
 function UsersSection() {
+  const[users , setUser] = useState ([])
+
+  useEffect(() => {
+    fetch(`https://randomuser.me/api/?results=10`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log("user data:", data)
+      setUser(data.results)
+    })
+  },[])
+  
   return (
     <section>
       <h2>Users Section</h2>
-      <div className="scroll-container"></div>
+      <div className="scroll-container">
+      <ul className="users-list">
+      {users.map((user,index)  => ( 
+        <li
+         className={user.gender === 'female'? "bg-blue" : "bg-pink"}>
+        <img src = {user.picture.medium} alt = {user.name} />
+        <h3>{`${user.name.title} ${user.name.first} ${user.name.last}`}</h3>
+        <p>Email: {user.email}</p>
+        </li> 
+      ))}
+      </ul>
+      </div>
     </section>
   )
 }
